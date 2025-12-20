8a35d4ce28baddd8ae68e1afac7b94c686f7fd70
shashwat558 <jainshashwat528@gmail.com>
2025-12-21 03:01:26 +0530

    added reader agent

--- app/agents/orchestrator.py
+++ app/agents/orchestrator.py
@@ -1,7 +1,7 @@
 from typing import TypedDict, Annotated, Sequence, List, Dict
 from langgraph.graph import StateGraph , END
 import datetime
-
+from reader_agent import reader_agent
 class ReaderOutput(TypedDict):
     summary: str
     scene: str
@@ -64,4 +64,33 @@ class AgentState(TypedDict):
     image_metadata: str
     page_number: int
     memory: dict
-    
+
+ 
+graph = StateGraph(AgentState)
+graph.add_node("reader_agent", reader_agent);
+
+graph.set_entry_point("reader_agent")
+app = graph.compile();
+initial_state = {
+    "page_number": 1,
+    "page_text": """
+    Alice walks into the abandoned library. Dust floats in the air.
+    She feels a strange presence watching her.
+    """,
+    "memory_agent_output": {
+        "story": "A mysterious journey through forgotten places",
+        "characters": {
+            "Alice": {
+                "appearance": "Young woman with a lantern",
+                "traits": ["curious", "brave"]
+            }
+        },
+        "relationships": {
+            "Alice": []
+        }
+    }
+}
+
+result = app.invoke(initial_state)
+
+
